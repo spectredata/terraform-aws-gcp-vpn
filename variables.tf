65e3c7e2ec946a079777caf0dcb3e1555695a75c
datadexer <63919187+datadexer@users.noreply.github.com>
2021-12-28 11:02:21 -0800

    Update variable types.

--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 variable "aws_cidr" {
   description = "CIDR group for AWS network"
-  type        = "string"
+  type        = string
 }
 
 variable "gcp_cidr" {
   description = "CIDR group for GCP network"
-  type        = "string"
+  type        = string
 }
 
 variable "gcp_network" {
   description = "Network name for GCP"
-  type        = "string"
+  type        = string
 }
 
 variable "gcp_region" {
   description = "Region for GCP"
-  type        = "string"
+  type        = string
 }
 
 variable "aws_region" {
   description = "Region for AWS"
-  type        = "string"
+  type        = string
 }
 
 variable "aws_vpc" {
   description = "VPC ID for AWS"
-  type        = "string"
+  type        = string
 }
 
 variable "aws_sg" {
   description = "Security group for AWS Network"
-  type        = "string"
+  type        = string
 }
 
 variable "aws_route_table_id" {
   description = "Routing table ID for AWS"
-  type        = "string"
+  type        = string
 }
